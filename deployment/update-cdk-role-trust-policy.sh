e7102302dcb1f42caf8cd815f1a25d78e19aa199
Christopher Watanabe <christopher.watanabe@brainlab.com>
2024-06-27 11:19:38 +0200

    feat: alMake CDK role trust relationship update conditional

--- deployment/update-cdk-role-trust-policy.sh
+++ deployment/update-cdk-role-trust-policy.sh
@@ -26,15 +26,34 @@ CODEBUILD_ROLE_NAME=$2
 ROLE_ARN_TO_ALLOW="arn:aws:iam::$ACCOUNT_ID:role/$CODEBUILD_ROLE_NAME"
 CDK_TOOLKIT_STACK_NAME="CDKToolkit"
 
+# Function to check if a role already has the CodeBuild role's ARN in its trust policy
+role_has_permission() {
+  local role_name=$1
+  local permission_arn=$2
+  local trust_policy
+  trust_policy=$(aws iam get-role --role-name "$role_name" --query "Role.AssumeRolePolicyDocument.Statement[?Principal.AWS == '$permission_arn']" --output json)
+  if [ -n "$trust_policy" ]; then
+    return 0 # Permission exists
+  else
+    return 1 # Permission does not exist
+  fi
+}
+
 # Get the list of IAM roles created by the CDK Toolkit stack
-ROLE_NAMES=$(aws cloudformation describe-stack-resources \
+CDK_ROLES=$(aws cloudformation describe-stack-resources \
   --stack-name $CDK_TOOLKIT_STACK_NAME \
   --query "StackResources[?ResourceType=='AWS::IAM::Role'].PhysicalResourceId" \
   --output text)
 
 # Loop through each role and update the trust relationship
-for ROLE_NAME in $ROLE_NAMES; do
-  echo "Updating trust relationship for role: $ROLE_NAME"
+for ROLE_NAME in $CDK_ROLES; do
+  echo "Updating trust relationship for role: $ROLE_NAME to allow CodeBuild Role to assume CDK roles"
+
+  # Check if the role already has the permission
+  if role_has_permission "$ROLE_NAME" "$ROLE_ARN_TO_ALLOW"; then
+    echo "Role $ROLE_NAME may already be assumed by CodeBuild Role."
+  else
+    echo "Adding trust relationship to $ROLE_NAME"
 
   # Get the existing trust relationship policy document
   TRUST_POLICY=$(aws iam get-role --role-name $ROLE_NAME --query "Role.AssumeRolePolicyDocument" --output json)
