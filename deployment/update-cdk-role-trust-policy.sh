68a4ac990f4f09b0500060e9e99e23ea12ee7200
Christopher Watanabe <christopher.watanabe@brainlab.com>
2024-06-28 19:51:08 +0200

    feat: update role trust policy update script

--- deployment/update-cdk-role-trust-policy.sh
+++ deployment/update-cdk-role-trust-policy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Script to add the CDH CodeBuild Role to the IAM role trust relationships created by the CDK Toolkit stack.
+# Script to add the CodeBuild role to the IAM role trust relationships created by the CDK Toolkit stack.
 # The CDK Toolkit stack is commonly done by running the bootstrap command. This script is necessary as it is
-# not currently possible to add custom parameters to the boostrap template and the CodeBuild role must be allowed
+# not currently possible to add custom parameters to the bootstrap template and the CodeBuild role must be allowed
 # to assume the CDK roles for CDK deployment from CodePipeline.
 #
 # Usage: update-cdk-role-trust-policy.sh <AccountId> <CodeBuildRoleName> <CdkToolkitStackName>
@@ -13,9 +13,9 @@
 #   CdkToolkitStackName The name of the CDK Toolkit stack
 #
 # Example:
-#   ./update-trust-relationships.sh 123456789012 env-CodeBuildRole cdk-toolkit-int1-fhirworks
-#
-# Check if both arguments are provided
+#   ./update-cdk-role-trust-policy.sh 123456789012 env-CodeBuildRole cdk-toolkit-int1-fhirworks
+
+# Check if all arguments are provided
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 <AccountId> <CodeBuildRoleName> <CdkToolkitStackName>"
   exit 1
@@ -33,7 +33,7 @@ role_has_permission() {
   local permission_arn=$2
   local trust_policy
   trust_policy=$(aws iam get-role --role-name "$role_name" --query "Role.AssumeRolePolicyDocument.Statement[?Principal.AWS == '$permission_arn']" --output json)
-  if [ -n "$trust_policy" ]; then
+  if [ -n "$trust_policy" ] && [ "$trust_policy" != "[]" ]; then
     return 0 # Permission exists
   else
     return 1 # Permission does not exist
@@ -42,9 +42,14 @@ role_has_permission() {
 
 # Get the list of IAM roles created by the CDK Toolkit stack
 CDK_ROLES=$(aws cloudformation describe-stack-resources \
-  --stack-name $CDK_TOOLKIT_STACK_NAME \
+  --stack-name "$CDK_TOOLKIT_STACK_NAME" \
   --query "StackResources[?ResourceType=='AWS::IAM::Role'].PhysicalResourceId" \
   --output text)
+
+if [ -z "$CDK_ROLES" ]; then
+  echo "No IAM roles found in stack $CDK_TOOLKIT_STACK_NAME"
+  exit 1
+fi
 
 # Loop through each role and update the trust relationship
 for ROLE_NAME in $CDK_ROLES; do
@@ -52,17 +57,28 @@ for ROLE_NAME in $CDK_ROLES; do
 
   # Check if the role already has the permission
   if role_has_permission "$ROLE_NAME" "$ROLE_ARN_TO_ALLOW"; then
-    echo "Role $ROLE_NAME may already be assumed by CodeBuild Role."
+    echo "Role $ROLE_NAME already allows assumption by CodeBuild Role."
   else
     echo "Adding trust relationship to $ROLE_NAME"
 
     # Get the existing trust relationship policy document
-    TRUST_POLICY=$(aws iam get-role --role-name $ROLE_NAME --query "Role.AssumeRolePolicyDocument" --output json)
+    TRUST_POLICY=$(aws iam get-role --role-name "$ROLE_NAME" --query "Role.AssumeRolePolicyDocument" --output json)
+
+    # Check if we successfully retrieved the trust policy
+    if [ -z "$TRUST_POLICY" ]; then
+      echo "Failed to get trust policy for role $ROLE_NAME"
+      continue
+    fi
 
     # Add the new role ARN to the trust policy
-    UPDATED_TRUST_POLICY=$(echo $TRUST_POLICY | jq --arg ROLE_ARN "$ROLE_ARN_TO_ALLOW" '.Statement += [{"Effect": "Allow", "Principal": {"AWS": $ROLE_ARN}, "Action": "sts:AssumeRole"}]')
+    UPDATED_TRUST_POLICY=$(echo "$TRUST_POLICY" | jq --arg ROLE_ARN "$ROLE_ARN_TO_ALLOW" '.Statement += [{"Effect": "Allow", "Principal": {"AWS": $ROLE_ARN}, "Action": "sts:AssumeRole"}]')
 
     # Update the trust relationship
-    aws iam update-assume-role-policy --role-name $ROLE_NAME --policy-document "$UPDATED_TRUST_POLICY"
+    aws iam update-assume-role-policy --role-name "$ROLE_NAME" --policy-document "$UPDATED_TRUST_POLICY"
+    if [ $? -ne 0 ]; then
+      echo "Failed to update trust policy for role $ROLE_NAME"
+    else
+      echo "Successfully updated trust policy for role $ROLE_NAME"
+    fi
   fi
 done
